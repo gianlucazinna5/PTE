aef5e2ce60cf5eadafbb841dbe0b8e5b2ef91a2c
gianlucazinna5 <gianluca.zinna96@gmail.com>
2021-10-29 15:11:10 +0200

    3

--- PTE/src/main/java/LinkArray.java
+++ PTE/src/main/java/LinkArray.java
@@ -23,26 +23,40 @@ public class LinkArray {
         Date data = new Date(System.currentTimeMillis());
     }
 
-//    ArrayList Links = new ArrayList();
-//
-//    public void AggiungiLink() {
-//        Links.forEach(() -> {
-//            Links.add();
-//        });
-//    }
-//
-//    public void Ordinamento() {
-//        for(int i = 0; i < Links.size(); i++) {
-//            int x = i;
-//            for(int j = i-1; j >= 0; j++) {
-//                if(((String) Links.get(j)).charAt(0) > ((String) Links.get(x)).charAt(0)) {
-//                    int k = (int) Links.get(x);
-//                    Links.set(x, Links.get(j));
-//                    Links.set(j, k);
-//                    x = j;
-//                }else break;
-//            }
-//        }
-//    }
+    ArrayList Links = new ArrayList();
+
+    public void AggiungiLink() {
+        Links.forEach(() -> {
+            Links.add();
+        });
+    }
+
+    public int MinCharString() {
+        int min = 0;
+        for(int i = 0; i < Links.size(); i++) {
+            if(Links.get(i).toString().length() < min) {
+                min = Links.get(i).toString().length();
+            }
+        }
+        return min;
+    }
+
+    public void scambio() {
+
+    }
+
+    public void Ordinamento() {
+        for(int i = 0; i < Links.size(); i++) {
+            int x = i;
+            for(int j = i-1; j >= 0; j++) {
+                if(((String) Links.get(j)).charAt(0) > ((String) Links.get(x)).charAt(0)) {
+                    int k = (int) Links.get(x);
+                    Links.set(x, Links.get(j));
+                    Links.set(j, k);
+                    x = j;
+                }else break;
+            }
+        }
+    }
 
 }
